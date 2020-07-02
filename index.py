78664360d9157c48f1fab21ed3c3bd0d10054802
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-02 10:43:18 +0300

    Add condinitional on extension

--- index.py
+++ index.py
@@ -1,3 +1,6 @@
+import pathlib
+from util import consts
+
 def read_f(path):
     with open(path, 'r') as f:
         data = f.readlines()
@@ -6,13 +9,17 @@ def read_f(path):
 
 symbol = '#'
 data = []
+path = 'README.md'
 
-for line in read_f('README.md'):
-    if line.startswith(symbol):
-        data.append(
-            {
-                'count': line.count(symbol),
-                'str': line[line.count(symbol) + 1:len(line) - 1],
-            }
-        )
-print(data)
+if pathlib.Path(path).suffix.lower() == consts.extension:
+    for line in read_f('README.md'):
+        if line.startswith(symbol):
+            data.append(
+                {
+                    'count': line.count(symbol),
+                    'str': line[line.count(symbol) + 1:len(line) - 1],
+                }
+            )
+    print(data)
+else:
+    raise Exception('This is not a Markdown file')
