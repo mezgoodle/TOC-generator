fdaf97105bd0bbbad137bd91635d5a9ffe7e3500
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-03 15:12:08 +0300

    Fix tests

--- tests/test_index.py
+++ tests/test_index.py
@@ -4,5 +4,5 @@ from util import consts, index
 def test_simple():
     index.main(consts.TEST_PATH)
     with open(f'{consts.TEST_PATH}/test.md', 'r') as file:
-            result = file.read()
+        result = file.read()
     assert result == consts.TEST_DATA
