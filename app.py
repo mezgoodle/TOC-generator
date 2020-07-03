f3877bb5e289ff6ddb24ab19dc9427bf696b5bf6
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-03 17:00:35 +0300

    Add files uploading

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask, render_template, request, url_for, redirect
+from werkzeug.utils import secure_filename
 from util.index import main
 from util import consts
 
-UPLOAD_FOLDER = './uploads'
+UPLOAD_FOLDER = './util'
 ALLOWED_EXTENSIONS = {'md'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = consts.MEMORY_LIMIT
 
 
 def allowed_file(filename):
@@ -21,8 +24,10 @@ def index():
     return render_template('index.html', rows=consts.ROWS)
 
 
-@app.route('/text', methods=['POST'])
+@app.route('/text', methods=['GET', 'POST'])
 def text():
+    if request.method == 'GET':
+        return redirect(url_for('index'))
     data = request.form['text']
     with open(f'{consts.PATH}/test.md', 'w') as file:
         file.write(data)
@@ -30,6 +35,17 @@ def text():
     with open(f'{consts.PATH}/test.md', 'r') as file:
         result = file.read()
     return render_template('index.html', rows=consts.ROWS, input=data, result=result)
+
+
+@app.route('/file', methods=['GET','POST'])
+def file():
+    if request.method == 'GET':
+        return redirect(url_for('index'))
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return 'All good'
 
 
 if __name__ == "__main__":
